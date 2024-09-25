5c8158a78dada0ff5e1d355526a59300ed3c666c
Vilius <va96721@gmail.com>
2024-09-25 10:06:11 +0100

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2492024WebLab
+{
+    internal class Employee
+    {
+        public String eName { get; set; }
+        public string eId { get; set; }
+        public double HoursWorked { get; set; }
+        public double HourlyRate { get; set; } = 9.5;
+
+        public Employee(string eName, string eId, double HoursWorked, double HourlyRate = 9.5)
+        {
+            this.eName = eName;
+            this.eId = eId;
+            this.HoursWorked = HoursWorked;
+            this.HourlyRate = HourlyRate;
+        }
+        public override string ToString()
+        {
+            return $"{eName}({eId})";
+        }
+        public double CalculateWage()
+        { 
+        return HoursWorked * HourlyRate;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using _2492024WebLab;
+
+var worker = new Employee("Koby Hills", "D65", 35);
+
+
+
+Console.WriteLine($"Salary is £ ${worker.CalculateWage()}");
+
+Console.WriteLine(worker.ToString());
