6f68905159ac3d44da114afac80fe821b27cb023
Vilius <va96721@gmail.com>
2024-09-26 09:06:47 +0100

    Part2 halfway done. missing Menu to tie everything down

--- Employee.cs
+++ Employee.cs
@@ -12,15 +12,15 @@ namespace _2492024WebLab
     {
         public String eName { get; set; }
         public string eId { get; set; }
-        public double HoursWorked { get; set; }
-        public double HourlyRate { get; set; } = 9.5;
+        public double hoursWorked { get; set; }
+        public double hourlyRate { get; set; } = 9.5;
 
         public Employee(string eName, string eId, double HoursWorked, double HourlyRate = 9.5)
         {
             this.eName = eName;
             this.eId = eId;
-            this.HoursWorked = HoursWorked;
-            this.HourlyRate = HourlyRate;
+            this.hoursWorked = HoursWorked;
+            this.hourlyRate = HourlyRate;
         }
 
         public override string ToString()
@@ -30,21 +30,68 @@ namespace _2492024WebLab
 
         public double CalculateWage()
         {
-            return HoursWorked * HourlyRate;
+            return hoursWorked * hourlyRate;
         }
 
-        public static bool NameValidation(string name)
+        public static bool nameValidation(string name)
         {
         return !string.IsNullOrWhiteSpace(name) && name.Length >= 1 && name.Length <=50;
         }
 
-        public static bool HourValidation(double WeeklyHours)
+        public static bool hourValidation(double WeeklyHours)
         {
             return WeeklyHours >= 1 && WeeklyHours <= 100;
         }
-        public static bool IdValidation(string id)
+        public static bool idValidation(string id)
         {
             return Regex.IsMatch(id, @"^[A-Za-z]\d{2}$");
         }
+        public static string getValidEmployeeName()
+        {
+            string name;
+            do
+            {
+                Console.Write("Enter Name (must be 1-50 characters): ");
+                name = Console.ReadLine();
+
+                if (!Employee.nameValidation(name))
+                {
+                    Console.WriteLine("Error: Invalid name");
+                }
+            } while (!Employee.nameValidation(name));
+            return name;
+        }
+        public static string getValidEmployeeId()
+        {
+            string id;
+            do
+            {
+                Console.Write("Please ender ID (Letter followed by two numbers): ");
+                id = Console.ReadLine();
+
+                if (!Employee.idValidation(id))
+                {
+                    Console.WriteLine("Error: Invalid ID");
+                }
+            } while (!Employee.idValidation(id));
+            return id;
+        }
+        public static double getValidEmployeeHours()
+        {
+            double hours;
+            do
+            {
+                Console.Write("Enter hours worked (1-100): ");
+                bool isValidNumber = double.TryParse(Console.ReadLine(), out hours);
+                if (!isValidNumber || !Employee.hourValidation(hours))
+                {
+                    Console.WriteLine("Error: Invalid Hours");
+                }
+            } while (!Employee.hourValidation(hours));
+            return hours;
+        }
+
+       
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,62 +2,47 @@
 
 internal class Program
 {
+    static List<Employee> employeeList = new List<Employee>();
     private static void Main(string[] args)
     {
+
+
         var worker = new Employee("Koby Hills", "D65", 35);
+        var workerOne = new Employee("Boby Bills", "B65", 35);
+        var workerTwo = new Employee("Hoby Kills", "H65", 35);
 
+        employeeList.Add(worker);
+        employeeList.Add((Employee)workerOne);  
+        employeeList.Add((Employee)workerTwo);
 
+        /*Console.WriteLine($"Salary is £ ${worker.CalculateWage()}");
 
-        Console.WriteLine($"Salary is £ ${worker.CalculateWage()}");
+        Console.WriteLine(worker.ToString());*/
 
-        Console.WriteLine(worker.ToString());
-        GetValidEmployeeName();
-        GetValidEmployeeId();
-        GetValidEmployeeHours();
+        addEmployee();
+        displayEmployees();
 
-        static string GetValidEmployeeName()
+        static void addEmployee()
         {
-            string name;
-            do
-            {
-                Console.Write("Enter Name (must be 1-50 characters): ");
-                name = Console.ReadLine();
+            string NewEmployeeName = Employee.getValidEmployeeName();
+            string newEmployeeId = Employee.getValidEmployeeId();
+            double hoursWored = Employee.getValidEmployeeHours();
 
-                if (!Employee.NameValidation(name))
-                {
-                    Console.WriteLine("Error: Invalid name");
-                }
-            } while (!Employee.NameValidation(name));
-            return name;
+            var newEmployee = new Employee(NewEmployeeName, newEmployeeId, hoursWored);
+
+            employeeList.Add(newEmployee);
+
+            Console.WriteLine($"Employee added:  {newEmployee.ToString()}");
         }
-        static string GetValidEmployeeId()
-        {
-            string id;
-            do
-            {
-                Console.Write("Please ender ID (Letter followed by two numbers): ");
-                id = Console.ReadLine();
 
-                if (!Employee.IdValidation(id))
-                {
-                    Console.WriteLine("Error: Invalid ID");
-                }
-            } while (!Employee.IdValidation(id));
-            return id;
-        }
-        static double GetValidEmployeeHours()
+        static void displayEmployees()
         {
-            double hours;
-            do
+            Console.WriteLine("List of Employees: ");
+            foreach (Employee employee in employeeList)
             {
-                Console.Write("Enter hours worked (1-100): ");
-                bool isValidNumber = double.TryParse(Console.ReadLine(), out hours);
-                if (!isValidNumber || !Employee.HourValidation(hours))
-                {
-                    Console.WriteLine("Error: Invalid Hours");
-                }
-            } while (!Employee.HourValidation(hours));
-            return hours;
+                Console.WriteLine(employee.ToString());
+
+            }
         }
     }
 }
