8ea203cfcc4249f8e94c07ee44e2e2cface4e3db
Vilius <va96721@gmail.com>
2024-09-26 11:11:43 +0100

    Menu done

--- Program.cs
+++ Program.cs
@@ -8,41 +8,65 @@ internal class Program
 
 
         var worker = new Employee("Koby Hills", "D65", 35);
-        var workerOne = new Employee("Boby Bills", "B65", 35);
-        var workerTwo = new Employee("Hoby Kills", "H65", 35);
+        var workerOne = new Employee("Boby Bills", "B65", 42);
+        var workerTwo = new Employee("Hoby Kills", "H65", 36);
 
         employeeList.Add(worker);
-        employeeList.Add((Employee)workerOne);  
+        employeeList.Add((Employee)workerOne);
         employeeList.Add((Employee)workerTwo);
 
-        /*Console.WriteLine($"Salary is £ ${worker.CalculateWage()}");
-
-        Console.WriteLine(worker.ToString());*/
-
-        addEmployee();
-        displayEmployees();
-
-        static void addEmployee()
+        bool Online = true;
+        while (Online)
         {
-            string NewEmployeeName = Employee.getValidEmployeeName();
-            string newEmployeeId = Employee.getValidEmployeeId();
-            double hoursWored = Employee.getValidEmployeeHours();
-
-            var newEmployee = new Employee(NewEmployeeName, newEmployeeId, hoursWored);
-
-            employeeList.Add(newEmployee);
-
-            Console.WriteLine($"Employee added:  {newEmployee.ToString()}");
-        }
-
-        static void displayEmployees()
-        {
-            Console.WriteLine("List of Employees: ");
-            foreach (Employee employee in employeeList)
+            Console.WriteLine("\nWelcome to the Employee Management Menu");
+            Console.WriteLine("1: Add an Employee");
+            Console.WriteLine("2: Delete Employee");
+            Console.WriteLine("3: Display all Employees");
+            Console.WriteLine("4: Quit");
+            string choice = Console.ReadLine();
+            switch (choice)
             {
-                Console.WriteLine(employee.ToString());
-
+                case "1":
+                    addEmployee(); break;
+                case "2":
+                    break;
+                case "3":
+                    displayEmployees(); break;
+                case "4":
+                    Online = false;
+                    Console.WriteLine("Cya next time"); break;
+                default:
+                    Console.WriteLine("Error: number between 1 and 4"); break;
             }
+
+
+            /*addEmployee();
+            displayEmployees();*/
+
+            static void addEmployee()
+            {
+                string NewEmployeeName = Employee.getValidEmployeeName();
+                string newEmployeeId = Employee.getValidEmployeeId();
+                double hoursWored = Employee.getValidEmployeeHours();
+
+                var newEmployee = new Employee(NewEmployeeName, newEmployeeId, hoursWored);
+
+                employeeList.Add(newEmployee);
+
+                Console.WriteLine($"Employee added:  {newEmployee.ToString()}");
+            }
+
+            static void displayEmployees()
+            {
+                Console.WriteLine("List of Employees: ");
+                foreach (Employee employee in employeeList)
+                {
+                    Console.WriteLine(employee.ToString());
+                    Console.WriteLine($"Wage is: ${employee.CalculateWage()}");
+
+                }
+            }
+
         }
     }
 }
